97c082583c9231fd9c6b9e503a5a02c2ba8dcbfd
Kamal Kishore <kamal.kishore@siemens.com>
2020-12-01 21:39:07 +0530

    m

--- src/main/java/com/dp/MatrixChainMultiplication.java
+++ src/main/java/com/dp/MatrixChainMultiplication.java
@@ -15,8 +15,8 @@ import com.dp.BooleanParenthesization;
  * 3)Evaluate Expression to true / boolean parenthesization
  * 4)Min/Max value of an Expr.
  * 5)Palindrom Partitioning |132. Palindrome Partitioning II, https://leetcode.com/problems/palindrome-partitioning-ii/
- * 6)Scramble String
- * 7)Egg Dropping Problem
+ * 6)Scramble String | 87. Scramble String https://leetcode.com/problems/scramble-string/
+ * 7)Egg Dropping Problem |887. Super Egg Drop https://leetcode.com/problems/super-egg-drop/
  * ---------------------------GENERAL FORMAT CODE------------------
  * 	int solveMCM(int[] arr, int i, int j) {
 		if (i > j)return 0;
@@ -39,13 +39,20 @@ import com.dp.BooleanParenthesization;
 public class MatrixChainMultiplication extends DynamicPrograming{
 	static int dp[][]=new int [100][100];//size depends on max constraints 
 	static int p[][]=new int [100][100];
+	static int egg[][]=new int [100][100];
+
 	static {for(int []t:dp)
 			Arrays.fill(t, -1);
 			for(int []t:p)
+				Arrays.fill(t, -1);
+			for(int []t:egg)
 				Arrays.fill(t, -1);}
 	
 	static Map<String,Integer> map=new HashMap<>(); 
     private static final String EMPTY_STRING = "";
+    
+	static Map<String,Boolean> scramblemap=new HashMap<>(); 
+
 
 	public static void main(String[] args) {
 			//We are given a array which contains a matrix dimension we have to return minimum cost(multiplication) of these matrix 
@@ -64,6 +71,11 @@ public class MatrixChainMultiplication extends DynamicPrograming{
 			//PS:-Input String given Output -No. of ways it eval to true, add bracket to string such that it evaluate to true  eg: ((T^F)&T) 
 			//String may consist of T=True ,F=False, | =Or, & =And ,^=XOR
 			System.out.println("3)Evaluate Expression to true ans:- "+evalExTBottomUp(s1,0,s1.length()-1,true)); 
+			String a="great",b="rgeat"; //two String given you can create binary tree and swap non leaf nodes child if by doing this its equal to secound string it return true else false
+			System.out.println("6)Scramble String ans:- "+scrambledStringRecursie(a,b)); 
+			int eggs=3,floor=5; //IP:-Eggs and floor given we need to identify threshold floor from which if we throw egg it will not break 
+			//we have to apply best technique in worst case to minimize no of attempts to find threshold floor
+			System.out.println("7)Egg Dropping Problem ans:- "+eggDropMemoizedOptimized(eggs,floor)); 
 
 	}
 
@@ -254,4 +266,116 @@ public class MatrixChainMultiplication extends DynamicPrograming{
 	      map.put(key, ans); //CHANGE
 		 return ans;
 	} 
+	
+	public static boolean scrambledStringRecursie(String a ,String b) {
+		if(a.length()!=b.length()) return false;
+		if(a.isEmpty() && b.isEmpty())return true;
+		 return scrambledMemoized(a,b);
+		//return scrambledSolve(a,b);
+	} 
+	
+	public static boolean scrambledSolve(String a ,String b) {
+		if(a.compareTo(b)==0)return true;//This mean there is 0 swap in string and both are equal a&b
+		if(a.length()<=1)return false;//if both are not equal above ,if Empty String is there in any a or b then return false
+		int n=a.length();//both are equal by now
+		boolean flag=false;
+		for(int i=1;i<=n-1;i++) {
+			if(scrambledSolve(a.substring(0, i), b.substring(n - i, n))  //Condition 1 where there is swapping
+					&& scrambledSolve(a.substring(i, n), b.substring(0, n - i)) || 
+							scrambledSolve(a.substring(0,i),b.substring(0,i)) //Condition 2 where No swapping
+							&& scrambledSolve(a.substring(i,n),b.substring(i,n))) {
+				flag=true;
+				break;
+			}
+		}
+		 return flag;
+	} 
+	
+	 public static boolean scrambledMemoized(String a, String b) {
+	        String key =a+"-"+b;
+	        if (a.compareTo(b) == 0) return true;
+	        int n = a.length();
+	        if (scramblemap.containsKey(key)) {
+	             return scramblemap.get(key);
+	        }
+	        if (n <= 1) return false;
+	        for (int i = 1; i < n; i++) {
+	           
+	           if( scrambledMemoized(a.substring(0, i), b.substring(n - i, n)) //n - i is because we need to compare same count for both if 2 chars left then 2 chars right
+	                && scrambledMemoized(a.substring(i, n), b.substring(0, n - i)) ||
+	              scrambledMemoized(a.substring(0, i), b.substring(0, i)) 
+	                && scrambledMemoized(a.substring(i, n), b.substring(i, n)))
+	            {
+	                scramblemap.put(key, true);
+	                return true;
+	            }
+	        }
+	      
+	        scramblemap.put(key, false);
+	        return false;
+	    }
+	 
+	 
+		private static int eggDropRecursive(int e, int f) {
+			if(f==0 || f==1)return f;//if no floor or single then attempts equal to floor
+			if(e==1)return f; //if single egg then we start dropping from first floor until it breaks or f ;
+			int min=Integer.MAX_VALUE;
+			
+			for(int k=1;k<=f;k++) {
+				int temp=Math.max(eggDropRecursive(e-1, k-1),eggDropRecursive(e,f-k))+1;  //+1 is for one attempt over Max is because we need to select for worst case ,One condition for break and one for not
+			
+				min=Math.min(min, temp);
+			}
+			
+			return min;
+		}
+		
+		private static int eggDropMemoized(int e, int f) {
+			if(f==0 || f==1)return f;
+			if(e==1)return f; 
+			if(egg[e][f]!=-1)return egg[e][f]; //Change 1
+			
+			int min=Integer.MAX_VALUE;
+			
+			for(int k=1;k<=f;k++) {
+				int temp=Math.max(eggDropRecursive(e-1, k-1),eggDropRecursive(e,f-k))+1;  
+			
+				min=Math.min(min, temp);
+			}
+			egg[e][f]=min; //Change 2
+			return min;
+		}
+		
+		private static int eggDropMemoizedOptimized(int e, int f) {
+			if(f==0 || f==1)return f;
+			if(e==1)return f; 
+			if(egg[e][f]!=-1)return egg[e][f]; //Change 1
+			
+			int min=Integer.MAX_VALUE;
+			
+			for(int k=1;k<=f;k++) {
+				int breaks,intact;//here we are checking those two inner calls also in matrix 
+				if(egg[e-1][k-1]!=-1)  
+				{
+					breaks=egg[e-1][k-1];
+				} 
+				else {
+					breaks=eggDropRecursive(e-1, k-1);
+					egg[e-1][k-1]=breaks;
+				}
+				if(egg[e][f-k]!=-1) 
+				{
+					intact=egg[e][f-k];
+				} 
+				else {
+					intact=eggDropRecursive(e,f-k);
+					egg[e][f-k]=intact;
+				}
+				int temp=Math.max(breaks,intact)+1;  
+			
+				min=Math.min(min, temp);
+			}
+			egg[e][f]=min; //Change 2
+			return min;
+		}
 }
