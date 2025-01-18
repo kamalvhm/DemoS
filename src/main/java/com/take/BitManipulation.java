c97b4a6a9b9639a4b8a42df2d3f477d2bace106f
Kamal <kamal.kishore@siemens.com>
2025-01-18 11:04:42 +0530

    update

--- src/main/java/com/take/BitManipulation.java
+++ src/main/java/com/take/BitManipulation.java
@@ -29,10 +29,125 @@ public class BitManipulation {
 		System.out.println("11) No. of bits need flip to convert start to goal  (3):- "+noOfBitsFlip(10,7));
 
 		System.out.println("12) Generate Power Set for [1,2,3]:- "+powerSet(new int[] {1,2,3}));
+		
+		int nums[]= {1,2,1,2,4};
+		System.out.println("13) Return Single No from input array  (4):- "+singleNoI(nums));
+		
+		int nums2[]= {1,2,1,2,1,2,4};//every no appears three times except one element 
+		System.out.println("14) Return Single No from input array  (4):- "+singleNoII(nums2));
 
+		int nums3[]= {2,4,2,14,3,7,7,3};//every no appears three times except one element 
+		System.out.println("15) Return two Single No from input array  (14,4):- "+singleNoIII(nums3));
+		
+		System.out.println("16) Xor for 1 to N (4):- "+xorItoN(4));
+		
+		System.out.println("17) Xor for Range 4 to 7 & (0):- "+xorRange(4,7));
+
+		System.out.println("18) Divide two No (3):- "+divide(13,4));
 
 	}
 	
+	private static int divide(int divident, int divisor) {
+		if(divident==divisor)return 1;
+		boolean sign=true;
+		if(divident>=0 && divisor<0)sign=false;
+		if(divident<0 && divisor>0)sign=false;
+		long n=Math.abs((long)divident);
+		long d=Math.abs((long)divisor);
+		int quotient=0;
+		while(n>=d) {
+			int cnt=0; 
+			while(n>=(d<<cnt+1))cnt++;
+			quotient+=(1<<cnt);
+			n=n-(d*(1<<cnt));
+		}
+		if(quotient==(1<<31) && sign)return Integer.MAX_VALUE;
+		if(quotient==(1<<31) && !sign)return Integer.MIN_VALUE;
+
+		return sign?quotient:(-1*quotient);
+	}
+	
+/**	public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        if (dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
+
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+
+        int quotient = 0;
+
+        while (absDividend >= absDivisor) {
+            long tempDivisor = absDivisor, multiple = 1;
+            while (absDividend >= (tempDivisor << 1)) {
+                tempDivisor <<= 1;
+                multiple <<= 1;
+            }
+            absDividend -= tempDivisor;
+            quotient += multiple;
+        }
+
+        return negative ? -quotient : quotient;
+    } */
+
+	private static int xorRange(int l, int r) {
+//		int ans=0;
+//		for(int i=l;i<=r;i++)ans^=i;
+//		return ans;
+		return xorItoN(l-1) ^ xorItoN(r);
+	}
+
+	private static int xorItoN(int n) {
+		if(n%4==1)return 1;
+		else if(n%4==2)return n+1;
+		else if(n%4==3)return 0;
+		return n;
+	}
+
+	private static String singleNoIII(int[] nums) {
+		long xorr=0;
+		for(int i=0;i<nums.length;i++) {
+			xorr=xorr^nums[i];
+		}
+		int rightMostBit=(int)((xorr & (xorr-1)) ^ xorr);
+		int b1=0,b2=0;
+		for(int i=0;i<nums.length;i++) {
+			if((nums[i] & rightMostBit)!=0)
+				b1=b1^nums[i];
+			else b2=b2^nums[i];
+		}
+		return b1+" "+b2;
+	}
+
+	private static int singleNoII(int[] nums) {
+		int n=nums.length;
+/**		int ans=0;
+		for(int bitIndex=0;bitIndex<=31;bitIndex++) {
+			int cnt=0;
+			for(int i=0;i<n;i++) {
+				if((nums[i] & (1<<bitIndex))!=0)cnt++; //check if bit is set or not
+			}
+			if(cnt%3==1) {  //if not multiple of 3 then this bit is set in our ans
+				ans=ans | (1<<bitIndex);
+			}
+		}
+		return ans; */
+		int once=0,twos=0;
+		for(int i=0;i<n;i++) {
+			once=(once ^ nums[i]) & ~ twos;
+			twos=(twos ^ nums[i]) & ~ once;
+		}
+		return once;
+	}
+
+	private static int singleNoI(int[] nums) {
+		int xor=0; 
+		for(int i:nums)
+			xor^=i;
+		return xor;
+	}
+
 	private static List<List<Integer>> powerSet(int[] a) {
 		int n=a.length;
 		int totalNoSubsets=1<<n; //2 power n by left shif
